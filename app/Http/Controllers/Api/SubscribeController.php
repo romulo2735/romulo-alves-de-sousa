b1c3c9556cfd18fa9a221900b54bec9a876c98b2
Matheus <matheuspsilva29@gmail.com>
2022-07-20 09:42:25 -0300

    Job unsubscribe

--- app/Http/Controllers/Api/SubscribeController.php
+++ app/Http/Controllers/Api/SubscribeController.php
@@ -9,10 +9,17 @@ use Illuminate\Http\Request;
 class SubscribeController extends Controller
 {
     public function subscribe(Job $job){
-        // $user = auth('sanctum')->user();
-        // $user->jobs()->attach($job->id);
-        
+
        $job->users()->attach(auth('sanctum')->user());
+
+       
+       return response()
+            ->json([], 200);
+    }
+
+    public function unsubscribe(Job $job){
+        $user = auth('sanctum')->user();
+        $user->jobs()->detach($job->id);
 
        
        return response()
--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::apiResource('users', \App\Http\Controllers\Api\UserController::class);
 Route::apiResource('jobs', \App\Http\Controllers\Api\JobController::class);
 
 Route::post('jobs/{job}/subscribe', [\App\Http\Controllers\Api\SubscribeController::class, 'subscribe'])->middleware('auth:sanctum');
+Route::post('jobs/{job}/unsubscribe', [\App\Http\Controllers\Api\SubscribeController::class, 'unsubscribe'])->middleware('auth:sanctum');
 
 Route::prefix('auth')->group(function(){
     Route::post('login', [\App\Http\Controllers\Auth\Api\LoginController::class, 'login']);
